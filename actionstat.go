51c0ad8078f827623ee4991da981f3699dddb11d
SeukWon Kang <kasworld@gmail.com>
2015-02-13 21:19:24 +0900

    from goguelike

---
+++ actionstat.go
@@ -0,0 +1,96 @@
+// stat package
+// total and lap support
+package actionstat
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type element struct {
+	Count int64
+	Time  time.Time
+}
+
+type ActionStat struct {
+	Total element
+	Laps  []element
+	mutex sync.Mutex
+}
+
+func intDurToStr(count int64, dur time.Duration) string {
+	return fmt.Sprintf("%v/%5.1f/s", count, float64(count)/dur.Seconds())
+}
+
+func (a ActionStat) String() string {
+	//defer a.UpdateLap()
+	dur := time.Now().Sub(a.Total.Time)
+	lastlap := a.Laps[len(a.Laps)-1]
+	lapcount, lapdur := a.Total.Count-lastlap.Count, time.Now().Sub(lastlap.Time)
+	//a.Total.Sub(a.Laps[len(a.Laps)-1])
+	return fmt.Sprintf("total:%v lap:%v",
+		intDurToStr(a.Total.Count, dur),
+		intDurToStr(lapcount, lapdur))
+}
+
+func NewActionStat() *ActionStat {
+	r := &ActionStat{
+		Total: element{0, time.Now()},
+		Laps:  make([]element, 1),
+	}
+	r.UpdateLap()
+	return r
+}
+
+func (a *ActionStat) PerSec(min, max float64) float64 {
+	dur := time.Now().Sub(a.Total.Time)
+	perSec := float64(a.Total.Count) / dur.Seconds()
+	if perSec > max {
+		perSec = max
+	}
+	if perSec < min {
+		perSec = min
+	}
+	return perSec
+}
+
+func (a *ActionStat) LapPerSec(min, max float64) float64 {
+	lastlap := a.Laps[len(a.Laps)-1]
+	lapcount, lapdur := a.Total.Count-lastlap.Count, time.Now().Sub(lastlap.Time)
+	lapPerSec := float64(lapcount) / lapdur.Seconds()
+	if lapPerSec > max {
+		lapPerSec = max
+	}
+	if lapPerSec < min {
+		lapPerSec = min
+	}
+	return lapPerSec
+}
+
+func (a *ActionStat) NewLap() {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	a.Laps = append(a.Laps, element{a.Total.Count, time.Now()})
+}
+
+func (a *ActionStat) UpdateLap() {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	a.Laps[len(a.Laps)-1] = element{a.Total.Count, time.Now()}
+}
+
+func (a *ActionStat) Inc() {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	a.Total.Count++
+}
+func (a *ActionStat) Add(n int64) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	a.Total.Count += n
+}
+
+func (a *ActionStat) GetCount() int64 {
+	return a.Total.Count
+}
